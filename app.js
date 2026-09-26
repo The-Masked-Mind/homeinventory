fc1f01acb490f3a69e9ace22f3fa7b04dc1e812c
The-Masked-Mind <buehmi85@gmail.com>
2026-09-26 21:17:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2238,16 +2238,6 @@ async function bestandAnzeigen(
     suchtext = ""
 ) {
 
-    console.log(
-        "BESTAND WIRD GELADEN FÜR:",
-        aktuellerHaushaltId
-    );
-
-
-async function bestandAnzeigen(
-    suchtext = ""
-) {
-
     if (!aktuellerHaushaltId) {
 
         stockContent.innerHTML = `
@@ -3493,15 +3483,7 @@ async function haushalteZurAuswahlLaden() {
 
                         aktuellerHaushalt =
                             neueMitgliedschaft.households;
-console.log(
-    "NEUER HAUSHALT NAME:",
-    aktuellerHaushalt.name
-);
 
-console.log(
-    "NEUE HAUSHALT ID:",
-    aktuellerHaushaltId
-);
 
                         if (currentHousehold) {
 
